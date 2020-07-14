ec0fe073a02a095412ebcc36f363a0a6647aae23
mreysei <maikelreyes96@hotmail.com>
2020-07-14 20:49:16 +0100

    :recycle: little refactor

--- src/actions/on-retrieve-points.ts
+++ src/actions/on-retrieve-points.ts
@@ -2,8 +2,7 @@ import { Logger } from "../services/logger/logger"
 import { Bot } from "../services/bot/bot";
 
 export const onRetrievePoints = async (bot: Bot, userId: string): Promise<String> => {
-  const databaseUser = await bot.getUser(userId);
-  const gratitude = databaseUser.gratitude;
+  const { gratitude } = await bot.getUser(userId);
   const message = `Te quedan *${gratitude.toGive} puntos* y has recibido *${gratitude.totalWeek} puntos* esta semana`
 
   Logger.onRetrievePoints()
