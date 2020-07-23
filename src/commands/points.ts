a0227667ba2d54bbf2074afb16e5a264b08b3d2a
mreysei <maikelreyes96@hotmail.com>
2020-07-23 10:34:44 +0100

    :recycle: little refactor

--- src/commands/points.ts
+++ src/commands/points.ts
@@ -1,8 +1,12 @@
 import { Bot } from "../services/bot/bot";
 import { Body } from "../services/api/api-body";
-import { onRetrievePoints } from "../actions/on-retrieve-points";
+import { Logger } from "../services/logger/logger";
 
 export const points = async (body: Body, response: any, bot: Bot) => {
-  const message = await onRetrievePoints(bot, body.user_id);
+  const user = bot.getSlackUser(body.user_id);
+  const { gratitude } = await bot.getUser(body.user_id);
+  const message = `Te quedan *${gratitude.toGive} puntos* y has recibido *${gratitude.totalWeek} puntos* esta semana`
+
   response.send(message);
+  Logger.onRetrievePoints(user?.real_name ?? "Any")
 }
