a207755b03ab24d63e475d8622b6fffa798eb2d7
mreysei <maikelreyes96@hotmail.com>
2020-07-29 10:08:39 +0100

    :bug: send public message when its a public channel

--- src/commands/random.ts
+++ src/commands/random.ts
@@ -7,12 +7,12 @@ import { User } from "../models/slack/user";
 export const random = async (body: Body, response: any, bot: Bot) => {
   try {
     const user = bot.getSlackUser(body.user_id);
-    const slackMembers = bot.getSlackUsers().filter((user: User) => !user.is_bot && !user.deleted);
-    const maxUsers = slackMembers.length;
+    const slackMembers: User[] = bot.getSlackUsers().filter((user: User) => !user.is_bot && !user.deleted);
+    const maxUsers: number = slackMembers.length;
     const defaultUsers = 1;
     const words: string[] = body.text.split(' ');
     const param: number = words.length > 0 ? Number.parseInt(words[0]) : defaultUsers;
-    const users = param > maxUsers ? maxUsers : param;
+    const users = isNaN(param) ? defaultUsers : (param > maxUsers ? maxUsers : param);
     const usersSelected: string[] = [];
 
     for (let i = 0; i < users; i++) {
@@ -21,12 +21,17 @@ export const random = async (body: Body, response: any, bot: Bot) => {
       slackMembers.splice(random, 1);
     }
 
-    if (usersSelected.length > 0) {
+    if (usersSelected.length === 0) {
       response.send(`Vaya, parece que no se ha obtenido ningún resultado ${Emojis.Pepe}`);
       Logger.onRandomError(new Error('Has not given any results'))
     } else {
-      const id = body.channel_id ?? body.team_id;
-      bot.writeMessage(id, `Estos son los ${users} usuarios random: ${Emojis.CongaParrot}\n${usersSelected.join(" ")}`)
+      const message = `Estos son los ${users} usuarios random: ${Emojis.CongaParrot}\n${usersSelected.join(" ")}`;
+      if (body.channel_name !== 'privategroup') {
+        bot.writeMessage(body.channel_id, message)
+        response.send();
+      } else {
+        response.send(message);
+      }
       Logger.onRandom(user?.real_name ?? "Someone");
     }
   } catch (error) {
