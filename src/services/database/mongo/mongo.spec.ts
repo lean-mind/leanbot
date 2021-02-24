cc2c440ac3c30df3fa898b0485a6294bfe5a2608
Sara Revilla <srevillab@gmail.com>
2021-02-24 14:46:33 +0000

    :construction: update tests for mongoDB WIP

--- src/services/database/mongo/mongo.spec.ts
+++ src/services/database/mongo/mongo.spec.ts
@@ -2,26 +2,31 @@ import { MongoClient } from "mongodb"
 import { config } from "../../../config"
 import { MongoDB } from "./mongo"
 
-jest.mock('mongodb')
-jest.mock("../../../config")
-
 describe('Service MongoDB: ', () => {
+  const mongodbConfig = config.database.mongodb
   const database = "test"
-  const uri = "mongodb://localhost"
-  const instance = new MongoClient(uri)
-  const mongodb = new MongoDB(instance)
-
-  beforeEach(() => {
+  let db: MongoDB
+  
+  beforeAll(() => {
     config.database.mongodb = {
-      database, 
-      uri,
+      database,
+      uri: mongodbConfig.uri
     }
-
-    instance.isConnected = jest.fn(() => true)
+    db = new MongoDB()
+    // poblar la bd de test
+  })
+  
+  beforeEach(() => {
   })
 
-  it.todo('should get communities')
+  afterAll(() => {
+    // vaciar la bd de test
+    config.database.mongodb = mongodbConfig
+  })
+
+  it.todo('should retrieve all registered communities')
   it.todo('should register a community')
-  it.todo('should save thanks')
-  it.todo('should get a list of thanks from last week')
+  it.todo('should save gratitude messages')
+  it.todo('should retrieve all gratitude messages')
+  it.todo('should retrieve gratitude messages from a given number of days')
 }) 
