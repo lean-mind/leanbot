68b0298b165607883a1b3e15eca979f48b367112
Sara Revilla <srevillab@gmail.com>
2021-03-12 10:12:05 +0000

    :recycle: refactor slack interactive props

--- src/services/platform/slack/props/interactive-props.ts
+++ src/services/platform/slack/props/interactive-props.ts
@@ -12,28 +12,21 @@ interface Dictionary<T> {
   [key: string]: T
 }
 
+const buttonMapper = {
+  getProps: getSlackButtonAction,
+  accept: true
+}
+
 export const getSlackInteractiveProps = async (body: SlackBody): Promise<InteractiveProps> => {
   const mapper: Dictionary<Action> = { // TODO: Pendiente refactor 
     ["thanks-confirmation"]: {
       getProps: getSlackThanksConfirmationProps,
       accept: body.payload.type === "view_submission"
     },
-    ["accept-coffee"]: {
-      getProps: getSlackButtonAction,
-      accept: true
-    },
-    ["reject-coffee"]: {
-      getProps: getSlackButtonAction,
-      accept: true
-    },
-    ["try-again-coffee"]: {
-      getProps: getSlackButtonAction,
-      accept: true
-    },
-    ["stop-coffee"]: {
-      getProps: getSlackButtonAction,
-      accept: true
-    }
+    ["accept-coffee"]: buttonMapper,
+    ["reject-coffee"]: buttonMapper,
+    ["try-again-coffee"]: buttonMapper,
+    ["stop-coffee"]: buttonMapper
   }
   
   let actionId = ""
