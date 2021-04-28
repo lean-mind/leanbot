1097e55c86c216bf21901fcb8b11af52c901450f
AiranSchez <airanschez0@gmail.com>
2021-04-28 12:01:58 +0100

    :recycle: refactor api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import { API } from './services/api/api'
+import { httpsApp } from './services/api/api'
 import { scheduler } from './scheduler'
+import { config } from "./config";
+import { Logger } from "./services/logger/logger";
 
-new API()
+const port: number = parseInt(config.apiPort)
+httpsApp.listen(port, () => Logger.onApiStart(port))
+
 scheduler()
 
--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -1,6 +1,5 @@
 import * as https from 'https';
 import * as fs from 'fs';
-import * as cors from 'cors';
 import { json, urlencoded } from 'body-parser';
 import { Logger } from '../logger/logger';
 import { config } from '../../config';
@@ -28,41 +27,34 @@ const getPlatformProps = async (platform: Platform, data, getProps) => {
   return await getProps(platform, data)
 }
 
-export class API {
-  private port: number = parseInt(config.apiPort)
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const app = require('express')()
 
-  constructor(
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    private instance = require('express')()
-  ) {
-    this.instance.use(json())
-    this.instance.use(urlencoded({ extended: true }))
-    this.instance.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
 
-    Endpoints.forEach(({ name, action, getProps }: EndpointInstance) => {
-      this.instance.post(name, async (request: any, response: any) => {
-        const data = await getPlatformData(request)
-        
-        if (config.maintenance) {
-          return response.send(`¡Estamos en mantenimiento, sentimos las molestias! ${Emojis.Construction}`)
-        }
-        
-        if (data) {
-          const props = await getPlatformProps(data.platform, data.data, getProps)
-          action(data.platform, props)
-          return response.send()
-        }
-        
-        const errorMessage = "Invalid request"
-        Logger.onError(`${errorMessage}: ${request}`)
-        return response.send(errorMessage)
-      })
-    })
+Endpoints.forEach(({ name, action, getProps }: EndpointInstance) => {
+  app.post(name, async (request: any, response: any) => {
+    const data = await getPlatformData(request)
 
-    https.createServer({
-      key: fs.readFileSync(process.env.HTTPS_KEY ?? ""),
-      cert: fs.readFileSync(process.env.HTTPS_CERT ?? ""),
-      ca: fs.readFileSync(process.env.HTTPS_CHAIN ?? "")
-    }, this.instance).listen(this.port, () => Logger.onApiStart(this.port))
-  }
-}
+    if (config.maintenance) {
+      return response.send(`¡Estamos en mantenimiento, sentimos las molestias! ${Emojis.Construction}`)
+    }
+
+    if (data) {
+      const props = await getPlatformProps(data.platform, data.data, getProps)
+      action(data.platform, props)
+      return response.send()
+    }
+
+    const errorMessage = "Invalid request"
+    Logger.onError(`${errorMessage}: ${request}`)
+    return response.send(errorMessage)
+  })
+})
+
+export const httpsApp = https.createServer({
+  key: fs.readFileSync(process.env.HTTPS_KEY ?? ""),
+  cert: fs.readFileSync(process.env.HTTPS_CERT ?? ""),
+  ca: fs.readFileSync(process.env.HTTPS_CHAIN ?? "")
+}, app)
