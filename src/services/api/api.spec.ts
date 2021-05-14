1299468a5442f1ed33644e3ee0a34c434381880c
Sara Revilla <srevillab@gmail.com>
2021-05-14 11:00:28 +0100

    :white_check_mark: turn api tests into todos

--- src/services/api/api.spec.ts
+++ src/services/api/api.spec.ts
@@ -4,19 +4,19 @@ import { httpsApp } from "./api";
 const request = supertest(httpsApp)
 
 describe('API', () => {
-  it('POST interactive', async done => {
+  it.todo('POST interactive', async done => {
     const response = await request.post('/interactive')
     expect(response.status).toBe(200)
     done()
   })
   
-  it('POST thanks', async done => {
+  it.todo('POST thanks', async done => {
     const response = await request.post('/thanks')
     expect(response.status).toBe(200)
     done()
   })
   
-  it('POST coffeeRoulette', async done => {
+  it.todo('POST coffeeRoulette', async done => {
     const response = await request.post('/coffeeRoulette')
     expect(response.status).toBe(200)
     done()
