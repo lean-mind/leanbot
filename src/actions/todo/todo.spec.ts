ba930e30aefc9542deb27799357bd32848e65d90
AiranSchez <airanschez0@gmail.com>
2021-05-17 14:14:19 +0100

    :construction: add test for /todo and /todo list

--- src/actions/todo/todo.spec.ts
+++ src/actions/todo/todo.spec.ts
@@ -8,7 +8,7 @@ import { ToDo } from "../../models/database/todo"
 import { Id } from "../../models/platform/slack/id"
 import { completeToDo } from "./complete-todo"
 import { CheckboxActionPropsBuilder } from "../../tests/builders/actions/todo-checkbox-action-props-builder"
-import { Factory } from "../../services/infrastructure/factory";
+import { Factory } from "../../services/infrastructure/factory"
 
 describe("To Do", () => {
   let i18n: I18n
@@ -39,6 +39,22 @@ describe("To Do", () => {
       expect(db.saveToDo).toBeCalled()
     })
     it.todo("should inform the user about usage if no todo text is provided")
+    it("should allow users to create todos for other users and inform them", async () => {
+      const userId = "test-user-id"
+      const assigneeId = "U0123456"
+      const text = "my first todo"
+      const props: TodoProps = TodoPropsBuilder({ userId, text: `<@${assigneeId}|irrelevant-username> ${text}` })
+
+      await todo(platform, props, db)
+
+      expect(platform.sendMessage).nthCalledWith(1, userId, i18n.translate("todo.toDoCreated", { todo: text }))
+      expect(db.saveToDo).toBeCalled()
+      expect(platform.sendMessage).nthCalledWith(
+        2,
+        assigneeId,
+        i18n.translate("todo.toDoAssigned", { todo: text, user: `<@${userId}>` })
+      )
+    })
   })
 
   describe("/todo list", () => {
@@ -50,11 +66,33 @@ describe("To Do", () => {
       const props: TodoProps = TodoPropsBuilder({ userId: userId.id, text })
 
       await todo(platform, props, db)
+      const expectedView = await platform.getView("toDoList", { userId, toDoList })
 
       expect(db.getToDos).toBeCalledWith(userId.id)
-      expect(platform.sendMessage).toBeCalledWith(userId.id, expect.anything())
+      expect(platform.sendMessage).toBeCalledWith(
+        userId.id,
+        expect.objectContaining({ blocks: expectedView["blocks"] })
+      )
     })
+    it("should inform that there is no todos", async () => {
+      const userId: Id = new Id("user-id")
+      const toDoList = []
+      db.getToDos = jest.fn(async (_) => toDoList)
+      const text = "list"
+      const props: TodoProps = TodoPropsBuilder({ userId: userId.id, text })
 
+      await todo(platform, props, db)
+      const expectedView = await platform.getView("toDoList", { toDoList, userId })
+
+      expect(db.getToDos).toBeCalledWith(userId.id)
+      expect(platform.sendMessage).toBeCalledWith(
+        userId.id,
+        expect.objectContaining({ blocks: expectedView["blocks"] })
+      )
+    })
+  })
+
+  describe("complete action", () => {
     it("should allow the user to complete a todo", async () => {
       const toDoId = "test-todo-id"
       const props = CheckboxActionPropsBuilder({ value: toDoId })
@@ -64,5 +102,6 @@ describe("To Do", () => {
       expect(platform.updateMessage).toBeCalledWith(props.responseUrl, expect.any(Object))
       expect(db.completeToDo).toBeCalledWith(toDoId)
     })
+    it.todo("should inform the creator user that an assigned task is completed")
   })
 })
